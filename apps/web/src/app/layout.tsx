b7114dbcded6eee669a9964fcaffeb330087da44
Avirup Ghosh <ag.avirup1997@gmail.com>
2024-12-06 12:52:16 +0530

    📦 using react 19 latest

--- apps/web/src/app/layout.tsx
+++ apps/web/src/app/layout.tsx
@@ -1,7 +1,6 @@
 import type { Metadata } from 'next';
 import { Inter } from 'next/font/google';
 import { cn } from '@repo/ui/utils';
-
 import '@repo/ui/global.css';
 
 export const inter = Inter({
